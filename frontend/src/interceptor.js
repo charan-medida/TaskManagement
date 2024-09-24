52968352ae44323a5007afc139a035c1b7d29a77
charan-medida <166228617+charan-medida@users.noreply.github.com>
2024-09-25 00:42:53 +0530

    Update interceptor.js

--- frontend/src/interceptor.js
+++ frontend/src/interceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
     const axiosInstance  = axios.create({
-        baseURL: 'http://localhost:5006'
+        baseURL: 'https://taskmanagement-1-6h4e.onrender.com'
     });
 
     axiosInstance.interceptors.request.use(
